bbe312e5aedb98945e802439719805998f2a69d9
Irek Żuchowski <45558765+irzu@users.noreply.github.com>
2019-11-06 21:28:25 +0100

    add travis

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="osmtilecalc",
-    version="0.1.0",
+    version="0.1.1",
     description="Calculates Open Street Map tile coordinates.",
     long_description=open(
         os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")
